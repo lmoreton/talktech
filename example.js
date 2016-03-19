ee128d9dbf851fb53737f32765ac310a1fe2eb68
BrunoSkel <brunorochaesilva@gmail.com>
2016-03-19 12:28:17 -0300

    dsdsd

--- example.js
+++ example.js
@@ -3,6 +3,17 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '187634337:AAEoZr3M3m9AzgRYJoaxsTWVCuL_GHAYTu4';
 // Setup polling way 
 var bot = new TelegramBot(token, {polling: true});
+
+//Video uploading state
+var uploadingState = false;
+var waitingForLogin = false;
+
+var sentVideo = null;
+var sentVideoFromId = null;
+
+
+var login = "";
+var password = "";
  
 // Matches /echo [whatever] 
 bot.onText(/\/echo (.+)/, function (msg, match) {
@@ -20,12 +31,51 @@ bot.onText(/youtube.com\/watch%?v=([_A-Za-z0-9-]+)/, function (msg, match) {
 // Any kind of message 
 bot.on('message', function (msg) {
   //var chatId = msg.chat.id;
+
   var fromId = msg.from.id;
+
+  if (waitingForLogin == true) {
+  	//Login requested.
+  	if (login == "") {
+  		var text = msg.text;
+  		//bot.sendMessage(fromId, "Login: "+text);
+  		login = text;
+  		bot.sendMessage(fromId, "Please type your YouTube password.");
+  	}
+  	
+  	else if (password == "") {
+  		var text = msg.text;
+  		//bot.sendMessage(fromId, "Login: "+text);
+  		password = text;
+  		bot.sendMessage(fromId, "Please type your YouTube password.");
+  		waitingForLogin = false;
+  		processVideo()
+  	}
+
+  	return;
+  }
+
   var video = msg.video;
   
-  if (video != null) {
-  	bot.sendMessage(fromId, "it's a video");
+  if (video != null && uploadingState == false) {
+  	sentVideo = video;
+  	bot.sendMessage(fromId, "Video recieved.");
+  	processVideo(msg);
   }
   //var photo = 'cats.png';
   //bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
 });
+
+function processVideo() {
+
+  	//bot.sendMessage(fromId, "Processing...");
+
+  	if (login == "" && password == "") {
+  		waitingForLogin = true;
+  		bot.sendMessage(sentVideoFromId, "User not logged in. Please type your YouTube username.");
+  		return;
+  	}
+
+  	bot.sendMessage(fromId, "Processing...");
+
+}
